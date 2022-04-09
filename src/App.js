efd4cc1ec3ee592cd2bcf42868dde92398fad17e
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-09 17:13:47 -0500

    Added assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Welcome to my PORTFOLIO!</h1>
       <p>Hi, my name is Addy Rodriguez and I am starting out my career as a software engineer. I just graduated from General Assembly and I am looking forward to applying everything I have learned.</p>
     </div>
