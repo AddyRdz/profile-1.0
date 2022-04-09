2ee7861f9c4fde6b6f0a03ffc018d2289aa7bae8
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-08 21:32:06 -0500

    Installed tailwind

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to my PORTFOLIO!</h1>
+      <p>Hi, my name is Addy Rodriguez and I am starting out my career as a software engineer. I just graduated from General Assembly and I am looking forward to applying everything I have learned.</p>
     </div>
   );
 }
