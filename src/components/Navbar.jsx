3c0a00980c08051c6d4d2ab31390f7be1af08c0d
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-10 11:52:24 -0500

    Some styling on mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         {/* Navbar Menu */}
-        <div>
-            <ul className='flex'>
+            <ul className='hidden md:flex'>
                 <li>Home</li>
                 <li>About</li>
                 <li>Skills</li>
@@ -14,15 +13,14 @@ const Navbar = () => {
                 <li>Resume</li>
                 <li>Contact</li>
             </ul>
-        </div>
-
+        
         {/* Hamburger Nav */}
-        <div className='hidden'>
+        <div className='md:hidden'>
             <FaBars/>
         </div>
 
         {/* Mobile Menu */}
-        <ul className='hidden'>
+        <ul className='absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
             <li>Home</li>
             <li>About</li>
             <li>Skills</li>
@@ -30,6 +28,10 @@ const Navbar = () => {
             <li>Resume</li>
             <li>Contact</li>
         </ul>
+
+        {/* Social Info */}
+        <div className='hidden'></div>
+
     </div>
   )
 }
