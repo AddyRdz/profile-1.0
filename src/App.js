83445cc73fc6820b6f2315d9bbd4188b29ee74fd
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-09 23:43:23 -0500

    creating navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      <h1>Welcome to my PORTFOLIO!</h1>
-      <p>Hi, my name is Addy Rodriguez and I am starting out my career as a software engineer. I just graduated from General Assembly and I am looking forward to applying everything I have learned.</p>
+      <Navbar/>
+      {/* <h1 className="text-2xl font-bold">Welcome to my PORTFOLIO!</h1>
+      <p>Hi, my name is Addy Rodriguez and I am starting out my career as a software engineer. I just graduated from General Assembly and I am looking forward to applying everything I have learned.</p> */}
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
 
-const navbar = () => {
+const Navbar = () => {
   return (
-    <div>navbar</div>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        {/* Navbar Menu */}
+        <div>
+            <ul className='flex'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Resume</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+
+        {/* Hamburger Nav */}
+        <div className='hidden'>
+            <FaBars/>
+        </div>
+
+        {/* Mobile Menu */}
+        <ul className='hidden'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Projects</li>
+            <li>Resume</li>
+            <li>Contact</li>
+        </ul>
+    </div>
   )
 }
 
-export default navbar
+export default Navbar
