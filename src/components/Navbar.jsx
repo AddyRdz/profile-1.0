9ebfd91735f98840360c4cbed8971b673c663893
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-11 17:25:12 -0500

    Added social icon on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,14 +36,31 @@ const Navbar = () => {
         </ul>
 
         {/* Social Info */}
-        <div className='flex fixed flex-col top-[35%] left-0'>
+        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
         <ul>
-            <li>
-                <a href="/">
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration=300'>
+                <a className='flex justify-between items-center w-full text-gray-300'
+                 href="/">
                     LinkedIn <FaLinkedin size={30} />
                 </a>
-      
-
+            </li>
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration=300'>
+                <a className='flex justify-between items-center w-full text-gray-300'
+                 href="/">
+                    GitHub <FaGithub size={30} />
+                </a>
+            </li>
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration=300'>
+                <a className='flex justify-between items-center w-full text-gray-300'
+                 href="/">
+                    Email <HiOutlineMail size={30} />
+                </a>
+            </li>
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration=300'>
+                <a className='flex justify-between items-center w-full text-gray-300'
+                 href="/">
+                    Resume <BsFillPersonLinesFill size={30} />
+                </a>
             </li>
         </ul>
 
