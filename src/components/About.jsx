17d63a1d908f3b53d6c3210bc359cb4381d33ec5
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-11 19:47:26 -0500

    About component mobile configuration

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div></div>
+    <div name='about' className='w-full h-screen bg-[#1b1ba5] text-[#ccd6f6]'>
+        <div className='flex flex-col justify center items-center w-full h-full'>
+            <div className='max-w [1000px] w-full grid grid-col-2 gap-8'>
+                <div className='sm:text-right pb-8 pl-4'>
+                   <p className='text-4xl font-bold inline border-b-4'>About</p> 
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
