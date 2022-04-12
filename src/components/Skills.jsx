91efcd3ee9f58bccf9d5a9a23940537c1a7c4583
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-11 21:31:07 -0500

    Created Skills component and imported images

---
+++ src/components/Skills.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import HTML from '../assets/html.png';
+import CSS from '../assets/css3.png';
+import PYTHON from '../assets/python.png';
+import React from '../assets/react.png';
+import Node from '../assets/node-js.png';
+import Bootstrap from '../assets/bootstrap.png';
+import Tailwind from '../assets/file_type_tailwind_icon_130128.png';
+
+const Skills = () => {
+  return (
+    <div name='skills'>
+        {/* Container */}
+        <div>
+            <div>
+                <p>Experience</p>
+                <p>These are the technologies I've dabbled with</p>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Skills
