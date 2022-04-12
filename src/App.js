ba9a5e12e62d3aa2c1ed3aaad2a6effe8535333a
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-11 19:13:32 -0500

    Created home component and styled

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div>
       <Navbar/>
       <Home/>
-      {/* <h1 className="text-2xl font-bold">Welcome to my PORTFOLIO!</h1>
-      <p>Hi, my name is Addy Rodriguez and I am starting out my career as a software engineer. I just graduated from General Assembly and I am looking forward to applying everything I have learned.</p> */}
+      
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,18 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div className= 'w-full h-screen bg-[#3737c5]'>Home
+    <div name='home' className= 'w-full h-screen bg-[#1b1ba5]'>Home
+
+    {/* Container */}
+    <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className="text-2xl font-bold">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]" >Addy Rodriguez</h1>
+      <p className='font-bold text-[#8f9ecc] py-4 max-w-[700px]'>Welcome to my portfolio, I am starting out my career as a software engineer. I just graduated from General Assembly and I am looking forward to applying everything I have learned. Please look around and make sure to reach out!</p>
+      <div>
+          <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-black'>See My Work <HiArrowNarrowRight className='ml-4'/></button>
+      </div>
+
+    </div>
     
     </div>
   )
