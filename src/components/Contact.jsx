a411ce505d79ba874a3d41da92b800cb5e2bcb85
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-12 01:25:17 -0500

    Worked on Contact Form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,8 +6,11 @@ const Contact = () => {
         <form action="" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 text-[#ccd6f6]'>Contact</p>
-                <p>You can reach me by submitting the form below or you can send me an email to addyrodriguez18@gmail.com</p>
+                <p className='text-[#ccd6f6] py-4'>You can reach me by submitting the form below or you can send me an email to addyrodriguez18@gmail.com</p>
             </div>
+            <input className='bg-[#ccd6f6] p-2' type='text' placeholder="Name" name='name' />
+            <input className='my-4 p-2 bg-[#ccd6f6]' placeholder="Email" name='email' />
+            <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
         </form>
     </div>
   )
