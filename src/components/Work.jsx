267a594bf5ba78bca0b12c89effde09a2fbdd8ee
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-11 23:14:05 -0500

    Set up work header

---
+++ src/components/Work.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen bg-[#1b1ba5] text-[#ccd6f6]'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 text-[#ccd6f6]'>Work</p>
+                <p className='py-6'>Here is some of my recent work</p>
+            </div>
+
+            <div>
+                <div>
+                    <div>
+                        <div>
+                            <span>
+
+                            </span>
+                            <div>
+                                <a href='/'>
+                                    <button></button>
+                                </a>
+                                <a href='/'>
+                                    <button></button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Work
