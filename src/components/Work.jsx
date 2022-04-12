c2abdbe4ee98ac18e54e763c1090962f39558aab
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-12 00:13:05 -0500

    added work examples

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -12,9 +12,28 @@ const Work = () => {
                 <p className='text-4xl font-bold inline border-b-4 text-[#ccd6f6]'>Work</p>
                 <p className='py-6'>Here is some of my recent work</p>
             </div>
-
+            {/* Big Container */}
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                {/* Item Inside Container */}
                 <div style={{backgroundImage:`url(${MyPetPro})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-black tracking-wider'>
+                            Python Application
+
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-black font-bold text-lg'>Website</button>
+                            </a>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-black font-bold text-lg'>Code
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div style={{backgroundImage:`url(${EBB})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                     {/* Hover Effects */}
                     <div className='opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl font-bold text-black tracking-wider'>
@@ -26,7 +45,44 @@ const Work = () => {
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-black font-bold text-lg'>Website</button>
                             </a>
                             <a href='/'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-black font-bold text-lg'>Code</button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-black font-bold text-lg'>Code
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div style={{backgroundImage:`url(${CodingPals})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-black tracking-wider'>
+                            React JS Application
+
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-black font-bold text-lg'>Website</button>
+                            </a>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-black font-bold text-lg'>Code
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div style={{backgroundImage:`url(${Blackjack})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-black tracking-wider'>
+                            HTML, CSS
+
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-black font-bold text-lg'>Website</button>
+                            </a>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-black font-bold text-lg'>Code
+                                </button>
                             </a>
                         </div>
                     </div>
