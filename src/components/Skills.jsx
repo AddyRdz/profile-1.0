376fc48267019a03fe752a7cc799b380de241a9c
Addy Rodriguez <rdz.33.a@gmail.com>
2022-04-11 22:37:58 -0500

    Fixed error container box

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import HTML from '../assets/html.png';
+import JAVASCRIPT from '../assets/javascript.png';
 import CSS from '../assets/css3.png';
 import PYTHON from '../assets/python.png';
 import REACT from '../assets/react.png';
@@ -18,12 +19,41 @@ const Skills = () => {
                 <p className='py-4'>These are the technologies I've dabbled with</p>
             </div>
 
-            <div className='w-full grid grid-cols-2 sm:grid-cold-4 gap-4 text-center py-8'>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
                 <div className='shadow-md shadow-[#81b3b3] hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto' src={HTML} alt="HTML icon"/>
                     <p className='my-4'>HTML</p>
                 </div>
-            </div>
+            
+            <div className='shadow-md shadow-[#81b3b3] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={JAVASCRIPT} alt="JAVASCRIPT icon"/>
+                    <p className='my-4'>JAVASCRIPT</p>
+                </div>
+                <div className='shadow-md shadow-[#81b3b3] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={CSS} alt="CSS icon"/>
+                    <p className='my-4'>CSS</p>
+                </div>
+                <div className='shadow-md shadow-[#81b3b3] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={PYTHON} alt="PYTHON icon"/>
+                    <p className='my-4'>PYTHON</p>
+                </div>
+                <div className='shadow-md shadow-[#81b3b3] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={REACT} alt="REACT icon"/>
+                    <p className='my-4'>REACT</p>
+                </div>
+                <div className='shadow-md shadow-[#81b3b3] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Node} alt="NODE icon"/>
+                    <p className='my-4'>NODE</p>
+                </div>
+                <div className='shadow-md shadow-[#81b3b3] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Bootstrap} alt="BOOTSTRAP icon"/>
+                    <p className='my-4'>BOOTSTRAP</p>
+                </div>
+                <div className='shadow-md shadow-[#81b3b3] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Tailwind} alt="TAILWIND icon"/>
+                    <p className='my-4'>TAILWIND</p>
+                </div>
+            </div>    
         </div>
 
     </div>
